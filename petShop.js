208017514c5ea2aed2d64ad01deb9b61d2261be5
vonnAlves <alberto420762@gmail.com>
2020-07-26 14:34:41 -0300

    Ajuste de Codigo

--- petShop.js
+++ petShop.js
@@ -2,15 +2,17 @@ const nomeEmpresa = "Animal Petshop";
 
 console.log(nomeEmpresa);
 
-let listaDePets = []
+let listaDePets = [];
+
+// funcao para adicionar pets , e puxar para a array lista de pets.
 const adicionarPet = (Nome, 
   Idade, 
   Raca,
   Sexo,
   Cuidador,
   Castrado, 
-Vacinado,
-  rga) => {
+  Vacinado,
+  rga) => {  
   let novoPet = {
     Nome,
     Idade,
@@ -24,11 +26,12 @@ Vacinado,
   listaDePets.push(novoPet);
 }
   
-adicionarPet("Flor", 5, "Gato", "F", "Vinny", false, false,12345);
+adicionarPet("Flor", 5, "Gato", "Feminino", "Vinny", false, false,12345);
 adicionarPet("toto", 4, "chow-chow", "Masculino", "Carlos", false, false, 12334);
 
-
-listaDePets.forEach((pet) => {
+//lista todos os pets dentro da array
+const listarPet = () =>{
+  listaDePets.forEach((pet) => {
     console.log("-".repeat(12));
     console.log("Nome Do Pet:" + pet.Nome);
     console.log("Idade Do Pet:" + pet.Idade);
@@ -39,23 +42,28 @@ listaDePets.forEach((pet) => {
     console.log("Vacinado:" + pet.Vacinado);
     console.log("Registro:" + pet.rga);
   })
+}
 
+//funcao para castrar um pet, quando chamada gera uma nova array, que retornara "true" para pet.Castrado;
 const castrarPet = petNumero => {
   listaDePets.map(function(){
-  return petNumero.Castrado = true;
-  });
+    return petNumero.Castrado = true;
+    });
 }
+//funcao para vacinar um pet, quando chamada gera uma nova array, que retornara "true" para pet.Vacinadp;
 const vacinarPet = petNumero =>{
-  petNumero.Vacinado
-    listaDePets.map(function(){
-      petNumero.Vacinado = true;
-      });
-  }
-
+  listaDePets.map(function(){
+    return petNumero.Vacinado = true;
+    });
+}
+//realiza o tipo de servico que ira ser prestado;
 const servico = (petNumero, tipoServico) => tipoServico(petNumero);
-servico(listaDePets[1],vacinarPet);
+  
+servico(listaDePets[1],castrarPet);
+listarPet();
 
-const encontraPet = (petRga) => {
+//verifica se pet esta cadastrado no sistema por meio de seu registro;
+const buscarPet = (petRga) => {
   let petAchado = listaDePets.find(
     (pet, numero) => !!(listaDePets[numero].rga == petRga)
   );
@@ -63,6 +71,6 @@ const encontraPet = (petRga) => {
   return petAchado != undefined;
 };
 
-console.log("existe", encontraPet(12344445));
+console.log("Pet existe:", buscarPet(12345));
 
 
